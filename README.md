c1248663bb0de802d7743d905c2291e5f84ac20d
Yorick Peterse <git@yorickpeterse.com>
2023-09-28 23:32:28 +0200

    Support both top and bottom Telescope prompts

--- README.md
+++ README.md
@@ -69,7 +69,23 @@ so:
 ```lua
 require('telescope').setup({
   defaults = {
+    -- These three settings are optional, but recommended.
+    prompt_prefix = '',
+    entry_prefix = ' ',
+    selection_caret = ' ',
+
+    -- This is the important part: without this, Telescope windows will look a
+    -- bit odd due to how borders are highlighted.
     layout_strategy = 'grey',
+    layout_config = {
+     -- The extension supports both "top" and "bottom" for the prompt.
+      prompt_position = 'top',
+
+      -- You can adjust these settings to your liking.
+      width = 0.6,
+      height = 0.5,
+      preview_width = 0.6,
+    },
   }
 })
 
--- lua/telescope/_extensions/grey.lua
+++ lua/telescope/_extensions/grey.lua
@@ -15,21 +15,38 @@ return require('telescope').register_extension({
       local layout =
         strats.horizontal(self, max_columns, max_lines, layout_config)
 
-      layout.prompt.borderchars = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
-      layout.results.borderchars = { '─', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+      local top = self.layout_config.prompt_position == 'top'
 
-      layout.results.line = layout.results.line - 1
-      layout.results.height = layout.results.height + 1
+      layout.prompt.borderchars = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+
+      if top then
+        layout.results.borderchars =
+          { '─', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+      else
+        layout.results.borderchars =
+          { ' ', ' ', '─', ' ', ' ', ' ', ' ', ' ' }
+      end
+
+      if top then
+        layout.results.line = layout.results.line - 1
+      else
+        layout.results.line = layout.results.line + 1
+      end
 
       if layout.preview then
         layout.prompt.width = layout.prompt.width + layout.preview.width + 2
-        layout.preview.line = layout.preview.line + 2
-        layout.preview.height = layout.preview.height - 2
+        layout.preview.line = layout.results.line
+        layout.preview.height = layout.results.height
         layout.preview.width = layout.preview.width + 1
         layout.preview.col = layout.preview.col - 1
-        layout.preview.title = ''
-        layout.preview.borderchars =
-          { '─', ' ', ' ', '│', '┬', ' ', ' ', ' ' }
+
+        if top then
+          layout.preview.borderchars =
+            { '─', ' ', ' ', '│', '┬', ' ', ' ', ' ' }
+        else
+          layout.preview.borderchars =
+            { ' ', ' ', '─', '│', ' ', ' ', ' ', '┴' }
+        end
       end
 
       return layout
