f4cb4b6d83faa3dd19b1d63f8e6360753006f687
Yorick Peterse <git@yorickpeterse.com>
2024-04-10 18:52:41 +0200

    Tweak pounce styles and rename "yellow"

--- colors/grey.lua
+++ colors/grey.lua
@@ -27,12 +27,12 @@ local light_green = '#dfeacc'
 local light_red = '#f2d3cd'
 local red = '#c4331d'
 local grey = '#5e5e5e'
-local light_grey = '#e3e3e3'
+local light_grey = '#e6e6e6'
 local border = '#cccccc'
 local highlight = '#eeeeee'
-local yellow = '#b37f02'
-local light_yellow = '#f9e595'
-local bright_yellow = '#f9efc7'
+local dark_yellow = '#b37f02'
+local yellow = '#f9db70'
+local light_yellow = '#f9eab3'
 local orange = '#a55000'
 local purple = '#5c21a5'
 local white = '#ffffff'
@@ -41,7 +41,7 @@ local cyan = '#007872'
 g.terminal_color_0 = black
 g.terminal_color_1 = red
 g.terminal_color_2 = green
-g.terminal_color_3 = yellow
+g.terminal_color_3 = dark_yellow
 g.terminal_color_4 = blue
 g.terminal_color_5 = purple
 g.terminal_color_6 = cyan
@@ -50,7 +50,7 @@ g.terminal_color_7 = grey
 g.terminal_color_8 = black
 g.terminal_color_9 = red
 g.terminal_color_10 = green
-g.terminal_color_11 = yellow
+g.terminal_color_11 = dark_yellow
 g.terminal_color_12 = blue
 g.terminal_color_13 = purple
 g.terminal_color_14 = cyan
@@ -67,8 +67,8 @@ local highlights = {
   WhiteOnBlue = { fg = white, bg = blue },
   WhiteOnOrange = { fg = white, bg = orange },
   WhiteOnRed = { fg = white, bg = red },
-  WhiteOnYellow = { fg = white, bg = yellow },
-  Yellow = { fg = yellow, bold = true },
+  WhiteOnYellow = { fg = white, bg = dark_yellow },
+  Yellow = { fg = dark_yellow, bold = true },
   Bold = { fg = black, bold = true },
   Boolean = { link = 'Keyword' },
   Character = { link = 'String' },
@@ -88,8 +88,8 @@ local highlights = {
   Folded = { link = 'Comment' },
   Identifier = { fg = black },
   Function = { fg = black },
-  IncSearch = { bg = light_yellow },
-  CurSearch = { link = 'IncSearch' },
+  IncSearch = { link = 'Search' },
+  CurSearch = { link = 'Search' },
   Include = { fg = black, bold = true },
   InstanceVariable = { fg = purple },
   Keyword = { fg = black, bold = true },
@@ -143,14 +143,14 @@ local highlights = {
   VertSplit = { fg = border },
   WinSeparator = { fg = border },
   Visual = { bg = light_grey },
-  WarningMsg = { fg = yellow, bold = true },
+  WarningMsg = { fg = dark_yellow, bold = true },
   Whitespace = { fg = border },
   WildMenu = { link = 'PmenuSel' },
   -- ALE
   ALEError = { fg = red, bold = true },
   ALEErrorSign = { fg = red, bold = true },
-  ALEWarning = { fg = yellow, bold = true },
-  ALEWarningSign = { fg = yellow, bold = true },
+  ALEWarning = { fg = dark_yellow, bold = true },
+  ALEWarningSign = { fg = dark_yellow, bold = true },
   -- ccc.nvim
   CccFloatNormal = { link = 'NormalFloat' },
   CccFloatBorder = { link = 'FloatBorder' },
@@ -165,7 +165,7 @@ local highlights = {
   DiffAdd = { bg = light_green },
   DiffChange = { bg = highlight },
   DiffDelete = { fg = red },
-  DiffText = { bg = bright_yellow },
+  DiffText = { bg = light_yellow },
   diffAdded = { link = 'DiffAdd' },
   diffChanged = { link = 'DiffChange' },
   diffFile = { fg = black, bold = true },
@@ -187,7 +187,7 @@ local highlights = {
   DiffviewFilePanelTitle = { fg = black, bold = true },
   DiffviewFilePanelInsertions = { fg = green },
   DiffviewFilePanelDeletions = { fg = red },
-  DiffviewStatusModified = { fg = yellow, bold = true },
+  DiffviewStatusModified = { fg = dark_yellow, bold = true },
   DiffviewStatusAdded = { fg = green, bold = true },
   DiffviewStatusCopied = { fg = green, bold = true },
   DiffviewStatusDeleted = { fg = red, bold = true },
@@ -212,8 +212,8 @@ local highlights = {
   hamlTag = { fg = black, bold = true },
   -- hop.nvim
   HopNextKey = { fg = red, bold = true },
-  HopNextKey1 = { fg = yellow },
-  HopNextKey2 = { fg = yellow },
+  HopNextKey1 = { fg = dark_yellow },
+  HopNextKey2 = { fg = dark_yellow },
   HopUnmatched = {},
   -- HTML
   htmlArg = { link = 'Identifier' },
@@ -250,17 +250,17 @@ local highlights = {
   luaFunction = { link = 'Keyword' },
   -- LSP
   DiagnosticUnderlineError = { underline = true, sp = red },
-  DiagnosticUnderlineWarn = { underline = true, sp = yellow },
+  DiagnosticUnderlineWarn = { underline = true, sp = dark_yellow },
   LspDiagnosticsUnderlineError = { link = 'DiagnosticUnderlineError' },
   LspDiagnosticsUnderlineWarning = { link = 'DiagnosticUnderlineWarn' },
   DiagnosticFloatingError = { fg = red, bg = grey_bg_light, bold = true },
   DiagnosticFloatingHint = { fg = black, bg = grey_bg_light, bold = true },
   DiagnosticFloatingInfo = { fg = blue, bg = grey_bg_light, bold = true },
-  DiagnosticFloatingWarn = { fg = yellow, bg = grey_bg_light, bold = true },
+  DiagnosticFloatingWarn = { fg = dark_yellow, bg = grey_bg_light, bold = true },
   DiagnosticError = { fg = red, bold = true },
   DiagnosticHint = { fg = grey, bold = true },
   DiagnosticInfo = { fg = blue, bold = true },
-  DiagnosticWarn = { fg = yellow, bold = true },
+  DiagnosticWarn = { fg = dark_yellow, bold = true },
   -- Make
   makeTarget = { link = 'Function' },
   -- Markdown
@@ -274,7 +274,7 @@ local highlights = {
   -- Neogit
   NeogitBranch = { fg = green, bold = true },
   NeogitBranchHead = { link = 'NeogitBranch' },
-  NeogitCommitViewHeader = { fg = yellow, bold = true },
+  NeogitCommitViewHeader = { fg = dark_yellow, bold = true },
   NeogitCursorLine = { bg = highlight },
   NeogitDiffAdd = { link = 'DiffAdd' },
   NeogitDiffAddHighlight = { link = 'NeogitDiffAdd' },
@@ -294,7 +294,7 @@ local highlights = {
   NeogitGraphBoldPurple = { fg = purple, bold = true },
   NeogitGraphBoldRed = { fg = red, bold = true },
   NeogitGraphBoldWhite = { fg = black, bold = true },
-  NeogitGraphBoldYellow = { fg = yellow, bold = true },
+  NeogitGraphBoldYellow = { fg = dark_yellow, bold = true },
   NeogitGraphCyan = { fg = cyan },
   NeogitGraphGray = { fg = grey },
   NeogitGraphGreen = { fg = green },
@@ -302,7 +302,7 @@ local highlights = {
   NeogitGraphPurple = { fg = purple },
   NeogitGraphRed = { fg = red },
   NeogitGraphWhite = { fg = black },
-  NeogitGraphYellow = { fg = yellow },
+  NeogitGraphYellow = { fg = dark_yellow },
   NeogitHunkHeader = { fg = blue },
   NeogitHunkHeaderHighlight = { link = 'NeogitHunkHeader' },
   NeogitPopupActionKey = { link = 'NeogitPopupOptionKey' },
@@ -345,8 +345,8 @@ local highlights = {
   rustFuncCall = { link = 'Identifier' },
   rustModPath = { link = 'Identifier' },
   -- pounce.nvim
-  PounceAccept = { fg = red, bold = true },
-  PounceAcceptBest = { fg = red, bold = true },
+  PounceAccept = { fg = black, bg = yellow, bold = true },
+  PounceAcceptBest = { link = 'PounceAccept' },
   PounceMatch = { bg = light_yellow },
   PounceUnmatched = {},
   PounceGap = { link = 'None' },
@@ -371,7 +371,7 @@ local highlights = {
   typescriptLogicSymbols = { link = 'Operator' },
   -- Telescope
   TelescopeBorder = { fg = border, bg = grey_bg_light },
-  TelescopeMatching = { fg = yellow, bold = true },
+  TelescopeMatching = { fg = dark_yellow, bold = true },
   TelescopePromptNormal = { fg = black, bg = grey_bg_light },
   TelescopePromptBorder = { fg = border, bg = grey_bg_light },
   TelescopePromptPrefix = { fg = black, bold = true },
