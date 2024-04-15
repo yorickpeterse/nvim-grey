14069c2f1cf8193622d078b862323a1e640af057
Yorick Peterse <git@yorickpeterse.com>
2024-04-15 02:50:39 +0200

    Use a single border again for floats
    Without borders the floats blend too much into the background.

--- lua/telescope/_extensions/grey.lua
+++ lua/telescope/_extensions/grey.lua
@@ -15,17 +15,20 @@ return require('telescope').register_extension({
       local layout =
         strats.horizontal(self, max_columns, max_lines, layout_config)
 
-      layout.prompt.borderchars = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
-      layout.results.borderchars = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+      layout.prompt.borderchars =
+        { '─', '│', '─', '│', '╭', '╮', '╯', '╰' }
+      layout.results.borderchars =
+        { '─', '│', '─', '│', '├', '┤', '╯', '╰' }
       layout.results.line = layout.results.line - 1
 
       if layout.preview then
         layout.prompt.width = layout.prompt.width + layout.preview.width + 2
         layout.preview.line = layout.results.line
         layout.preview.height = layout.results.height
-        layout.preview.width = layout.preview.width
-        layout.preview.col = layout.preview.col
-        layout.preview.borderchars = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+        layout.preview.width = layout.preview.width + 1
+        layout.preview.col = layout.preview.col - 1
+        layout.preview.borderchars =
+          { '─', '│', '─', '│', '┬', '┤', '┘', '┴' }
       end
 
       return layout
@@ -34,16 +37,19 @@ return require('telescope').register_extension({
     strats.grey_cursor = function(self, max_columns, max_lines, layout_config)
       local layout = strats.cursor(self, max_columns, max_lines, layout_config)
 
-      layout.prompt.borderchars = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
-      layout.results.borderchars = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+      layout.prompt.borderchars =
+        { '─', '│', '─', '│', '┌', '┐', '┘', '└' }
+      layout.results.borderchars =
+        { '─', '│', '─', '│', '├', '┤', '┘', '└' }
 
       if layout.preview then
         layout.prompt.width = layout.prompt.width + layout.preview.width + 2
         layout.preview.line = layout.results.line
         layout.preview.height = layout.results.height
-        layout.preview.width = layout.preview.width
-        layout.preview.col = layout.preview.col
-        layout.preview.borderchars = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+        layout.preview.width = layout.preview.width + 1
+        layout.preview.col = layout.preview.col - 1
+        layout.preview.borderchars =
+          { '─', '│', '─', '│', '┬', '┤', '┘', '┴' }
       end
 
       return layout
