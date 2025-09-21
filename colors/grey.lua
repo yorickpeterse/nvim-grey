6042cba15aef73c49da211c73488fec432ceb4df
Yorick Peterse <yorick@yorickpeterse.com>
2025-09-21 03:30:48 +0200

    Make DiffDelete light grey
    This keeps it in line with the diffview highlights, and makes removed
    lines less painful to look at.

--- colors/grey.lua
+++ colors/grey.lua
@@ -174,7 +174,7 @@ local highlights = {
   -- Diffs
   DiffAdd = { bg = light_green },
   DiffChange = { bg = highlight },
-  DiffDelete = { fg = red },
+  DiffDelete = { fg = light_grey },
   DiffText = { bg = light_yellow },
   diffAdded = { link = 'DiffAdd' },
   diffChanged = { link = 'DiffChange' },
